0e53abff6ffef406d5b2b822a97a242b15875c62
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2022-10-29 13:04:38 +0300

    Add files via upload

---
+++ ProiectFinal/src/Screen.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Screen extends JFrame{
+    private JPanel panelTop;
+    private JPanel panelLeft;
+    private JPanel panelRigth;
+    private JList listaMasini;
+    private JButton salveazaNouButton;
+    private JButton editButton;
+    private JTextField textModel;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JPanel panelMain;
+    private JLabel labelModel;
+    private ArrayList<Masina> arr;
+
+    Screen(){
+            this.setContentPane(this.panelMain);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            arr = new ArrayList<Masina>();
+
+        salveazaNouButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String model= textModel.getText();
+                String pret = textField2.getText();
+                String an = textField3.getText();
+                String proprietar = textField4.getText();
+                if(!model.isEmpty() && !pret.isEmpty() && !an.isEmpty() && !proprietar.isEmpty()){
+                    Masina m = new Masina(model,proprietar,pret,an);
+                    arr.add(m);
+                    refreshList();
+                    labelModel.setText("adaugare reusita");
+                    //System.out.println(arr.toString());
+                }
+                else{
+                    labelModel.setText("Campul nume nu poate fi gol");
+                }
+
+            }
+        });
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        listaMasini.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+            }
+        });
+
+    }
+    private void refreshList(){
+
+        listaMasini.setListData(arr.toArray());
+    }
+
+    public void adaugaMasina(Masina m){
+        arr.add(m);
+    }
+
+        public static void main(String args[]){
+        Screen screen = new Screen();
+        screen.setVisible(true);
+
+
+        }
+}
