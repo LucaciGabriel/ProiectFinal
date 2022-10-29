a75198f6539f85242a50626dc13f26392030b248
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2022-10-29 16:25:16 +0300

    Add files via upload

--- ProiectFinal/src/Screen.java
+++ ProiectFinal/src/Screen.java
@@ -4,7 +4,7 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import AutoVehiculPackage.AutoVehicul;
+import AutoVehiculPackage.*;
 
 public class Screen extends JFrame{
     private JPanel panelTop;
