053dcb5e5aef9680c7a3dd413f88bd5e0e812c48
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2022-10-29 16:25:37 +0300

    Add files via upload

---
+++ ProiectFinal/src/AutoVehiculPackage/Utilitar.java
@@ -0,0 +1,15 @@
+package AutoVehiculPackage;
+
+import java.util.ArrayList;
+
+public class Utilitar {
+      public static double mediePreturi(ArrayList<AutoVehicul> arr){
+          int i=0;
+          double sum=0;
+          for(i=0;i<arr.size() && arr.get(i) != null;i++){
+               sum += arr.get(i).getPret();
+          }
+          return sum/arr.size();
+      }
+
+}
