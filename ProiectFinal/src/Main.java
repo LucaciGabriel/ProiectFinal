0afa850420cf17b23ff257be2d45446d2ef3dc96
came989 <45994084+came989@users.noreply.github.com>
2022-10-29 17:32:54 +0300

    Add files via upload

---
+++ ProiectFinal/src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Main extends JFrame{
+   private JPanel panel;
+    private JPanel panel1;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+
+    public Main(){
+        this.setContentPane(panel1);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       this.setSize(800,800);
+       //this.setVisible(true);
+    }
+
+}
