df740455700bd35fbf8e116e3c3eedd1ad97a834
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2022-10-30 12:50:40 +0200

    Add files via upload

--- ProiectFinal/src/Screen1.java
+++ ProiectFinal/src/Screen1.java
@@ -4,21 +4,21 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import AutoVehiculPackage.*;
-public class Screen2 extends Main{
+public class Screen1 extends Main{
     private JTextField textFieldModel;
     private JTextField textFieldProprietar;
     private JTextField textFieldPret;
     private JTextField textFieldAn;
-    private JTextField textFielVitezMaxima;
+    private JTextField textFieldNumarLocuri;
     private JButton buttonIntroducere1;
     private JLabel lblmodel;
     private JLabel lblproprietar;
     private JLabel lblpret;
     private JLabel lblan;
-    private JLabel lblvitezamaxima;
+    private JLabel lblnumarlocuri;
     private JLabel lblerror;
     JPanel ScreenPanel;
-    public Screen2(ArrayList<Motocicleta> list,JList jlist,DefaultListModel listmodel)
+    public Screen1(ArrayList<Masina> list,JList jlist,DefaultListModel listmodel)
     {
         buttonIntroducere1.addActionListener(new ActionListener() {
             @Override
@@ -27,16 +27,16 @@ public class Screen2 extends Main{
                 String proprietar= textFieldProprietar.getText();
                 int pret= Integer.parseInt(textFieldPret.getText());
                 int an=Integer.parseInt(textFieldAn.getText());
-                int vitezamaxima=Integer.parseInt(textFielVitezMaxima.getText());
-                if(!model.isEmpty() && !proprietar.isEmpty() && pret>0 && an>0 && vitezamaxima>0) {
-                    list.add(new Motocicleta(model, proprietar, pret, an, vitezamaxima));
-                    listmodel.addElement("* "+model+"-"+proprietar+"-"+pret+"-"+an+"-"+vitezamaxima);
+                int numarlocuri=Integer.parseInt(textFieldNumarLocuri.getText());
+                if(!model.isEmpty() && !proprietar.isEmpty() && pret>0 && an>0 && numarlocuri>0) {
+                    list.add(new Masina(model, proprietar, pret, an, numarlocuri));
+                    listmodel.addElement("* "+model+"-"+proprietar+"-"+pret+"-"+an+"-"+numarlocuri);
                     jlist.setModel(listmodel);
                     textFieldModel.setText("");
                     textFieldProprietar.setText("");
                     textFieldPret.setText("0");
                     textFieldAn.setText("0");
-                    textFielVitezMaxima.setText("0");
+                    textFieldNumarLocuri.setText("0");
                     lblerror.setText("S-a introdus cu succes");
                 }
                 else lblerror.setText("Date Incorecte(unul sau mai multe campuri goale)");
--- ProiectFinal/src/Screen3.java
+++ ProiectFinal/src/Screen3.java
@@ -2,59 +2,46 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
 import AutoVehiculPackage.*;
+public class Screen3 extends Main{
+    private JTextField textFieldModel;
+    private JTextField textFieldProprietar;
+    private JTextField textFieldPret;
+    private JTextField textFieldAn;
+    private JTextField textFieldCapacitate;
+    private JButton buttonIntroducere1;
+    private JLabel lblmodel;
+    private JLabel lblproprietar;
+    private JLabel lblpret;
+    private JLabel lblan;
+    private JLabel lblcapacitate;
+    private JLabel lblerror;
+    JPanel ScreenPanel;
 
-public class Screen3 extends JFrame{
-
-    private JPanel panel1;
-    private JList list1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JButton salveazaMotocicleteButton;
-    private JTextField textField5;
-    private JLabel labelModel;
-    private ArrayList<Motocicleta> arrMotocicleta;
-
-    public Screen3() {
-        this.setContentPane(this.panel1);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        arrMotocicleta = new ArrayList<Motocicleta>();
-
-        salveazaMotocicleteButton.addActionListener(new ActionListener() {
+    public Screen3(ArrayList<Camion> list,JList jlist,DefaultListModel listmodel)
+    {
+        buttonIntroducere1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String model= textField1.getText();
-                String pret = textField2.getText();
-                String an = textField3.getText();
-                String proprietar = textField4.getText();
-                String vitezaMaxima = textField5.getText();
-                if(!model.isEmpty() && !pret.isEmpty()  && !proprietar.isEmpty()){
-                    int tempPret = Integer.parseInt(textField2.getText());
-                    int tempAn = Integer.parseInt(textField3.getText());
-                    int tempVitMax=Integer.parseInt(textField5.getText());
-                    Motocicleta moto = new Motocicleta(model,proprietar,tempPret,tempAn,tempVitMax);
-                    arrMotocicleta.add(moto);
-                    refreshList();
-                    labelModel.setText("adaugare reusita");
-                    //System.out.println(arr.toString());
+                String model= textFieldModel.getText();
+                String proprietar= textFieldProprietar.getText();
+                int pret= Integer.parseInt(textFieldPret.getText());
+                int an=Integer.parseInt(textFieldAn.getText());
+                int capacitate=Integer.parseInt(textFieldCapacitate.getText());
+                if(!model.isEmpty() && !proprietar.isEmpty() && pret>0 && an>0 && capacitate>0) {
+                    list.add(new Camion(model, proprietar, pret, an, capacitate));
+                    listmodel.addElement("* "+model+"-"+proprietar+"-"+pret+"-"+an+"-"+capacitate);
+                    jlist.setModel(listmodel);
+                    textFieldModel.setText("");
+                    textFieldProprietar.setText("");
+                    textFieldPret.setText("0");
+                    textFieldAn.setText("0");
+                    textFieldCapacitate.setText("0");
+                    lblerror.setText("S-a introdus cu succes");
                 }
-                else{
-                    labelModel.setText("Campul nume nu poate fi gol");
-                }
-
+                else lblerror.setText("Date Incorecte(unul sau mai multe campuri goale)");
             }
         });
-
-        private void refreshList(){
-
-            list1.setListData(arrMotocicleta.toArray());
-        }
-        public void adaugaAutovehicul(Motocicleta moto){
-            arrMotocicleta.add(moto);
-        }
-
     }
-
 }
